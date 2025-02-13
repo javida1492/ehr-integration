1a016d9d4ccd1d71f083a18b3879f05089f12b0a
Andrew Javidfar <javida1492@gmail.com>
2025-02-13 01:10:48 -0500

    Update patient routes

--- backend/src/routes/patientRoutes.js
+++ backend/src/routes/patientRoutes.js
@@ -5,11 +5,9 @@ const patientController = require("../controllers/patient")
 // Patient CRUD endpoints
 router.post("/", patientController.createPatient)
 router.get("/:patient_id", patientController.getPatient)
+router.get("/", patientController.getAllPatients)
 router.put("/:patient_id", patientController.updatePatient)
 router.delete("/:patient_id", patientController.deletePatient)
-
-// New endpoint to get all patients
-router.get("/", patientController.getAllPatients)
 
 // Patient answers endpoints
 router.post("/:patient_id/answers", patientController.submitAnswer)
